e06bc0c7017de85d01d964579a2f6a9b063bbc9a
NITHISHguviii <nithishkumarv0612@gmail.com>
2024-07-28 17:38:30 +0530

    changed

--- app.js
+++ app.js
@@ -1,17 +1,25 @@
-const express=require('express')
-const app=express()
-const cors=require('cors')
-const bodyparser=require('body-parser')
-const AdminRoutes=require("./Routes/admin")
-const ClientRoutes=require("./Routes/client")
-const Image=require('./Routes/image')
-require("dotenv").config()
-app.use(cors())
-app.use(bodyparser.urlencoded({extended:true}))
-app.use(bodyparser.json())
+const express = require("express");
+const app = express();
+const cors = require("cors");
+const bodyparser = require("body-parser");
+const AdminRoutes = require("./Routes/admin");
+const ClientRoutes = require("./Routes/client");
+const Image = require("./Routes/image");
+require("dotenv").config();
+app.use(
+  cors({
+    origin: "*", // Allow all origins
+    methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
+    allowedHeaders: ["Content-Type", "Authorization"],
+  })
+);
+
+app.options("*", cors()); // Enable pre-flight requests for all routes
+app.use(bodyparser.urlencoded({ extended: true }));
+app.use(bodyparser.json());
 app.use(AdminRoutes);
-app.use(ClientRoutes)
-app.use(Image)
-app.listen(4000,()=>{
-    console.log("Server started 4000")
-})
+app.use(ClientRoutes);
+app.use(Image);
+app.listen(4000, () => {
+  console.log("Server started 4000");
+});
