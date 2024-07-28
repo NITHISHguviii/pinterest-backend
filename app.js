ed2111754281017685a83ac5b26671cf8b0fb243
NITHISHguviii <nithishkumarv0612@gmail.com>
2024-07-28 17:51:59 +0530

    changed url

--- app.js
+++ app.js
@@ -6,14 +6,12 @@ const AdminRoutes = require("./Routes/admin");
 const ClientRoutes = require("./Routes/client");
 const Image = require("./Routes/image");
 require("dotenv").config();
-app.use(
-  cors({
-    origin: "*", // Allow all origins
-    methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
-    allowedHeaders: ["Content-Type", "Authorization"],
-  })
-);
-
+const corsOptions = {
+  origin: "https://pinterest-backend-1wai.onrender.com", // Replace with your frontend domain
+  methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
+  allowedHeaders: ["Content-Type", "Authorization"],
+};
+app.use(cors(corsOptions));
 app.options("*", cors()); // Enable pre-flight requests for all routes
 app.use(bodyparser.urlencoded({ extended: true }));
 app.use(bodyparser.json());
